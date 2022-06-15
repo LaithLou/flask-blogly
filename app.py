769e302ffd6e7588208d1227270493b36ad2813d
LaithLou <laithlouay@gmail.com>
2022-06-15 15:16:09 -0400

    adding homepage rout and class user

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
+from flask import Flask, request, render_template
+from models import db, connect_db , User
 
-from flask import Flask
-from models import db, connect_db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -10,3 +10,13 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+
+@app.get('/')
+def list_user():
+    """ it lists users """
+
+    users = User.query.all()
+    return render_template('list.html',users = users)
+
+
--- models.py
+++ models.py
@@ -1,1 +1,30 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+  """Connect to database."""
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+
+     __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+    first_name = db.Column(db.String(),
+                        nullable = False)
+    last_name = db.Column(db.String(),
+                        nullable = False)
+    img_url = db.Column(db.String(),
+                        nullable= True,
+                        unique =True,
+                        default='')
+
